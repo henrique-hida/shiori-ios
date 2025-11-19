fdebe272500415678f12b9647cd181fe40b4e4d1
Henrique Hida <hmhida@icloud.com>
2025-11-19 01:43:51 -0300

    [feat] add unfinished SignIn View

--- Shiori/Views/SignInView/SignInViewModel.swift
+++ Shiori/Views/SignInView/SignInViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+@Observable
+class SignInViewModel {
+    let userService: UserService
+    
+    init(userService: UserService) {
+        self.userService = userService
+    }
+    
+    var email: String = ""
+    var password: String = ""
+    var isPasswordVisible: Bool = false
+    var goToHomeView: Bool = false
+    var goToSignUpView: Bool = false
+    
+    func togglePassword() {
+        isPasswordVisible.toggle()
+    }
+    
+    func login() async throws {
+        Task {
+            do {
+                try await userService.signIn(email: email, password: password)
+                
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func goToSignUp() {
+        goToSignUpView = true
+    }
+}

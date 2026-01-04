4f566a0ad4111e77f6f37ccec1ec71ee4fcf7a1f
Henrique Hida <hmhida@icloud.com>
2026-01-04 01:18:56 -0300

    [test] create email validator and it's tests

---
+++ ShioriTests/Mocks/MockAuthRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockAuthRepository.swift
+//  ShioriTests
+//
+//  Created by Henrique Hida on 03/01/26.
+//
+
+import Foundation
---
+++ ShioriTests/SignInViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  ShioriTests.swift
+//  ShioriTests
+//
+//  Created by Henrique Hida on 07/11/25.
+//
+
+import Testing
+
